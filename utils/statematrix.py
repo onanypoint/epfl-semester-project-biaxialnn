8d156a38c2bafb0b360c1adae6ab2b4681df2fd5
onanypoint <onanypoint@gmail.com>
2016-12-08 19:25:08 +0100

    Implement StateMatrixBuilder

--- utils/statematrix.py
+++ utils/statematrix.py
@@ -183,3 +183,122 @@ class StateMatrixBuilder(object):
             Has to be implemented in children class
         """
         raise NotImplementedError()
+
+class StateMatrixBuilderSimple(StateMatrixBuilder):
+    """Minimal statematrix builder
+    
+    This builder construct statematrix (or stream) using only the play and 
+    articulations informations. If going from state to stream, builds a 
+    music21.stream object with the minimal construction based on a straight 
+    forward approach.
+
+    """
+
+    @property
+    def information_count(self):
+        return 2
+
+    def stream_to_statematrix(self, stream):
+        fy = lambda n: n.pitch.ps
+
+        s = stream.flat
+
+        duration = max(s.highestTime, s.duration.quarterLength)
+        quantization_normalized = int(quantization / 4)
+        duration_quantized = int(math.ceil(duration * quantization / 4)) + 1
+        span = upperBound - lowerBound
+
+        statematrix = np.zeros((duration_quantized, span, self.information_count))
+        max_time = 0
+
+        for obj in s.flat.getElementsByClass((m21.meter.TimeSignature, m21.note.Note, m21.chord.Chord)):
+            valueObjPairs = []
+            if isinstance(obj, m21.note.Note):
+                valueObjPairs = [(fy(obj), obj)]
+
+            elif isinstance(obj, m21.chord.Chord):
+                values = self._extract_chord_data(fy, obj)
+                valueObjPairs = [(v, obj) for v in values]
+
+            elif isinstance(obj, m21.meter.TimeSignature):
+                if obj.numerator not in (2, 4):
+                    print("Found time signature event {}. Bailing!".format(
+                        obj.ratioString))
+                    return statematrix[:max_time]
+                continue
+
+            for v, objSub in valueObjPairs:
+                numericValue = m21.common.roundToHalfInteger(v)
+
+                if numericValue % 1 != 0:
+                    print("Warning: note has been rounded from",
+                          numericValue, "to", int(numericValue))
+                    numericValue = int(numericValue)
+
+                if (numericValue < lowerBound) or (numericValue >= upperBound):
+                    print("Note {} at time {} out of bounds (ignoring)".format(
+                        numericValue, objSub.offset))
+                    pass
+
+                else:
+                    start = objSub.offset
+                    length = objSub.quarterLength
+                    tie = self._get_note_tie(objSub)
+
+                    start_quant = int(start * quantization_normalized)
+                    length_quant = int(length * quantization_normalized)
+                    end_quant = start_quant + length_quant
+                    max_time = max(max_time, end_quant)
+
+                    if start_quant != end_quant:
+                        statematrix[start_quant:end_quant, numericValue - lowerBound, 0] = 1
+
+                    if not tie or tie == 'start':
+                        statematrix[start_quant, numericValue - lowerBound, 0:2] = 1
+                        
+        return statematrix[:max_time]
+
+    def statematrix_to_stream(self, statematrix, name='untitled'):
+        s = m21.stream.Score()
+        
+        s.insert(0, m21.metadata.Metadata())
+        s.metadata.title = name
+        s.metadata.composer = 'Biaxial neural network'
+        s.metadata.date = m21.metadata.DateSingle(str(datetime.datetime.now().year))
+        
+        p = m21.stream.Part()
+        
+        for i, values in enumerate(statematrix.transpose((1,0,2))):
+            pitch = i + lowerBound
+        
+            note = None
+            time = 0
+            
+            for t, values in enumerate(values):
+                play, art = values[0:2]
+                dynamics = values[-2:-1]
+                
+                if art and play:
+                    if note:
+                        note.quarterLength = time/4.0
+                        p.insert(start/4.0, note)
+                    
+                    note = m21.note.Note(pitch)
+                    time = 0
+                    start = t
+                
+                if not play and note:
+            
+                    note.quarterLength = time/4.0
+                    p.insert(start/4.0, note)   
+                    note = None
+                    time = 0
+                    start = None
+                
+                if play:
+                    time = time + 1
+                    
+        p.makeMeasures(inPlace=True) 
+        s.append(p)
+        
+        return s
